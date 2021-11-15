9100330ceefd428e1a620c091dcc05b68cb08a7f
Anish Hirlekar <anish.hirlekar@metamug.com>
2021-11-16 00:27:33 +0530

    Added comment metadata

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,32 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext, useState } from '@forge/ui';
+import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext, useState
+, Strong, User, Em } from '@forge/ui';
 import api, { route } from "@forge/api";
 
 const App = () => {
 	const context = useProductContext();
 	const [comments] = useState(async () => await fetchCommentsForIssue(context.platformContext.issueKey));
 	const [changes] = useState(async () => await fetchChangelogForIssue(context.platformContext.issueKey));
-	
+	let lastComment = null;
+	if(comments.length > 0) {
+		lastComment = comments[comments.length-1]
+	}
 //	console.log(`Number of comments on this issue: ${comments.length}`);
 
   	return (
 	    <Fragment>
-	    	<Fragment>
 		      <Text>
-		        Number of comments on this issue: {comments.length}
+		        <Strong>Number of comments:</Strong> {comments.length}
 		      </Text>
-			   
-		    </Fragment>
-	      	<Fragment>
+		      {lastComment !== null ?
+
+		      	<Text>Last commented by {lastComment.author.displayName}:
+		      		<Em>"{lastComment.body.content[0].content[0].text}"</Em>
+		      	</Text>
+		      	: null
+		      }
 		      <Text>
 		      	Changes done since issue creation: {changes.length}
 		      </Text>
-		    <Fragment>
 	    </Fragment>
 	);
 };
